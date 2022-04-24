c916257588e42a62a389686e965d6d87efa5f414
Alex <alex.convolk@ya.ru>
2022-04-24 20:46:51 +0300

    Add some games in catalog

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,20 @@
 import os
+import json, requests
 
+import steam.webapi
+from steam import steamid, webauth, webapi
+
+from steam.steamid import SteamID
+from steam.webapi import WebAPI, WebAPIInterface, WebAPIMethod
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, jsonify
 from flask_restful import Api, Resource, abort, reqparse
 from flasgger import Swagger, swag_from
 
-steam_web_api_key = "71C62A93E943CF496209037648DA088D"
+steam_web_api_key: str
+with open("C:/STUDYING/VI/Технологии программирования/steam_key.txt") as path:
+    steam_web_api_key = path.read()
+steam_id = SteamID(76561198080385483)
 secret_key = os.urandom(16).hex()
 
 app = Flask(__name__)
@@ -16,6 +26,10 @@ app.config['SWAGGER'] = {
     'specs_route': '/swagger/'
 }
 
+steam_api = WebAPI(
+    steam_web_api_key,
+    # format="vdf",
+)
 api = Api(app)
 swagger = Swagger(app)
 
@@ -33,15 +47,24 @@ def index():
 
 
 @app.route('/catalog', methods=['GET'])
-# @swag_from('test.yml')
+@swag_from('test.yml')
 def catalog():
-    return render_template('catalog.html', secret_key=app.secret_key)
+    apps = [240, 440, 570, 620, 730]
+    page_format = "json"
+    # page = f"https://api.steampowered.com/ISteamApps/GetAppList/v2/?key={steam_web_api_key}&format={page_format}"
+    # soup = BeautifulSoup(requests.get(response).text, features="html.parser")
+    # games = json.loads(soup.text)['applist']['apps']
+    response = steam_api.call("ISteamApps.GetAppList", format=page_format)
+    games = response['applist']['apps']
+    games = [games[_index] for _index in range(len(games)) if games[_index]["appid"] in apps]
+    # games = json.dumps()
+    return render_template('catalog.html', games=games, secret_key=app.secret_key)
 
 
-@app.route('/game-page', methods=['GET'])
+@app.route('/game/<int:appid>&<string:appname>', methods=['GET'])
 # @swag_from('test.yml')
-def game_page():
-    return render_template('game-page.html', secret_key=app.secret_key)
+def game_page(appid, appname):
+    return render_template('game-page.html', appid=appid, appname=appname, secret_key=app.secret_key)
 
 
 @app.route('/helper', methods=['GET'])
