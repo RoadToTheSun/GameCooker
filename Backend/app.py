019e6c57c7eef70212e475fd7f9a2b302091c568
Alex <alex.convolk@ya.ru>
2022-04-24 14:08:04 +0300

    Add client side with page navigation

--- Backend/app.py
+++ Backend/app.py
@@ -20,21 +20,64 @@ api = Api(app)
 swagger = Swagger(app)
 
 
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
-
-
-@app.route('/', methods=['GET'])
-@swag_from('test.yml')
-def index():
-    return render_template('index.html', secret_key=app.secret_key)
-
-
 @app.route('/api/<string:language>/', methods=['GET'])
 @swag_from('test.yml')
 def test(language):
     return jsonify(language=language)
+
+
+@app.route('/', methods=['GET'])
+# @swag_from('test.yml')
+def index():
+    return render_template('index.html', secret_key=app.secret_key)
+
+
+@app.route('/catalog', methods=['GET'])
+# @swag_from('test.yml')
+def catalog():
+    return render_template('catalog.html', secret_key=app.secret_key)
+
+
+@app.route('/game-page', methods=['GET'])
+# @swag_from('test.yml')
+def game_page():
+    return render_template('game-page.html', secret_key=app.secret_key)
+
+
+@app.route('/helper', methods=['GET'])
+# @swag_from('test.yml')
+def helper():
+    return render_template('helper.html', secret_key=app.secret_key)
+
+
+@app.route('/profile', methods=['GET'])
+# @swag_from('test.yml')
+def profile():
+    return render_template('profile.html', secret_key=app.secret_key)
+
+
+@app.route('/authors', methods=['GET'])
+# @swag_from('test.yml')
+def authors():
+    return render_template('authors.html', secret_key=app.secret_key)
+
+
+@app.route('/login', methods=['GET'])
+# @swag_from('test.yml')
+def login():
+    return render_template('login.html', secret_key=app.secret_key)
+
+
+@app.route('/registrate', methods=['GET'])
+# @swag_from('test.yml')
+def registrate():
+    return render_template('registration.html', secret_key=app.secret_key)
+
+
+@app.route('/pass-change', methods=['GET'])
+# @swag_from('test.yml')
+def pass_change():
+    return render_template('pass-change.html', secret_key=app.secret_key)
 
 
 if __name__ == '__main__':
